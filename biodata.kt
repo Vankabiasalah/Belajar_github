fcf2220d20e1c4afbe7deeacf2b6a38387167c1e
Vankabiasalah <145539844+Vankabiasalah@users.noreply.github.com>
2023-09-20 15:10:04 +0700

    Create biodata.kt

---
+++ biodata.kt
@@ -0,0 +1,13 @@
+fun main()
+{
+   var nama = "Ryandra rauvanka akbar"
+   var kelas = "XI RPL 2"
+   var hobi = "Write Code"
+
+  println("nama =$nama") 
+  println("kelas =$kelas") 
+  println("hobi =$hobi") 
+   
+}
+   
+   
